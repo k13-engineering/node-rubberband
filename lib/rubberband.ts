653eb2ba52b54da98a6cecdc22542f1dda9565ae
Simon Kadisch <simon.kadisch@k13-engineering.at>
2026-01-17 16:31:53 +0100

    Fix loading of WASM - browser not supported for now

--- lib/rubberband.ts
+++ lib/rubberband.ts
@@ -1,10 +1,23 @@
-import { loadAsBlob } from "esm-resource";
+const isNode = typeof globalThis.process !== "undefined" &&
+  globalThis.process.versions !== null &&
+  globalThis.process.versions.node !== null;
+
+const loadRubberbandWasmNodeJs = async () => {
+  const nodeFs = await import("fs");
+  const nodeUrl = await import("url");
+  const rubberbandWasmUrl = import.meta.resolve("rubberband-wasm/dist/rubberband.wasm");
+  const rubberbandWasmPath = nodeUrl.fileURLToPath(rubberbandWasmUrl);
+  const rubberbandWasm = await nodeFs.promises.readFile(rubberbandWasmPath);
+  return new Uint8Array(rubberbandWasm);
+};
 
 const loadRubberbandWasm = async () => {
-  const rubberbandBlob = await loadAsBlob({ importMeta: import.meta, filepath: "../node_modules/rubberband-wasm/dist/rubberband.wasm" });
-  const rubberbandWasmArrayBuffer = await rubberbandBlob.arrayBuffer();
-  const rubberbandWasm = new Uint8Array(rubberbandWasmArrayBuffer);
-  return rubberbandWasm;
+
+  if (isNode) {
+    return await loadRubberbandWasmNodeJs();
+  }
+
+  throw Error("browser environment not implemented yet");
 };
 
 export {
