5e9238dbd93f16fc8b3dbf037f4a06031cd75d51
Simon Kadisch <simon.kadisch@k13-engineering.at>
2026-01-17 14:50:20 +0100

    Initial implementation

---
+++ lib/index.ts
@@ -0,0 +1,125 @@
+import { RubberBandInterface } from "rubberband-wasm";
+import { loadRubberbandWasm } from "./rubberband.ts";
+
+type TRubberbandAudioData = {
+  planes: Float32Array[];
+};
+
+const rubberbandWasm = await loadRubberbandWasm();
+
+const wasm = await WebAssembly.compile(rubberbandWasm);
+const rubberbandApi = await RubberBandInterface.initialize(wasm);
+
+const OptionProcessRealTime = 1;
+
+const createRubberbandWrapper = ({
+  sampleRate,
+  channelCount,
+  maxBufferSizeInFrames
+}: {
+  sampleRate: number;
+  channelCount: number;
+  maxBufferSizeInFrames: number;
+}) => {
+
+  const channelArrayPtr = rubberbandApi.malloc(channelCount * 4);
+  const channelDataPtrs: number[] = [];
+  for (let channel = 0; channel < channelCount; channel += 1) {
+    const bufferPtr = rubberbandApi.malloc(maxBufferSizeInFrames * 4);
+    channelDataPtrs.push(bufferPtr);
+    rubberbandApi.memWritePtr(channelArrayPtr + channel * 4, bufferPtr);
+  }
+
+  const rb = rubberbandApi.rubberband_new(sampleRate, channelCount, OptionProcessRealTime, 1, 1);
+
+  const requestPitchScale = ({ pitchScale }: { pitchScale: number }) => {
+    rubberbandApi.rubberband_set_pitch_scale(rb, pitchScale);
+  };
+
+  const requestTimeRatio = ({ timeRatio }: { timeRatio: number }) => {
+    rubberbandApi.rubberband_set_time_ratio(rb, timeRatio);
+  };
+
+  const samplesRequired = () => {
+    return rubberbandApi.rubberband_get_samples_required(rb);
+  };
+
+  const process = ({ audioData }: { audioData: TRubberbandAudioData }) => {
+
+    const firstPlane = audioData.planes[0];
+    const sampleCount = firstPlane.length;
+
+    audioData.planes.forEach((plane, channelIndex) => {
+
+      if (plane.length > maxBufferSizeInFrames) {
+        throw new Error("plane length exceeds max buffer size");
+      }
+
+      const channelDataPtr = channelDataPtrs[channelIndex];
+      rubberbandApi.memWrite(channelDataPtr, plane);
+    });
+
+    rubberbandApi.rubberband_process(rb, channelArrayPtr, sampleCount, 0);
+  };
+
+  const available = (): number => {
+    return rubberbandApi.rubberband_available(rb);
+  };
+
+  const latencyInSamples = (): number => {
+    return rubberbandApi.rubberband_get_latency(rb);
+  };
+
+  // eslint-disable-next-line complexity
+  const retrieve = ({ sampleCount }: { sampleCount: number }): TRubberbandAudioData => {
+
+    if (sampleCount > maxBufferSizeInFrames) {
+      throw Error(`requested sample count ${sampleCount} exceeds max buffer size ${maxBufferSizeInFrames}`);
+    }
+
+    if (sampleCount > available()) {
+      throw Error(`look before you leap: requested ${sampleCount} samples but only ${available()} available`);
+    }
+
+    const samplesReceived = rubberbandApi.rubberband_retrieve(rb, channelArrayPtr, sampleCount);
+
+    if (samplesReceived > sampleCount) {
+      throw Error("BUG: received more samples than requested");
+    }
+
+    if (samplesReceived < sampleCount) {
+      throw Error("BUG: received fewer samples than requested");
+    }
+
+    let planes: Float32Array[] = [];
+
+    channelDataPtrs.forEach((channelDataPtr) => {
+      const plane = rubberbandApi.memReadF32(channelDataPtr, samplesReceived);
+      planes = [...planes, plane];
+    });
+
+    return {
+      planes
+    };
+  };
+
+  return {
+    requestPitchScale,
+    requestTimeRatio,
+    samplesRequired,
+    process,
+    available,
+    latencyInSamples,
+    retrieve
+  };
+};
+
+type TRubberbandWrapper = ReturnType<typeof createRubberbandWrapper>;
+
+export {
+  createRubberbandWrapper
+};
+
+export type {
+  TRubberbandWrapper
+};
---
+++ lib/rubberband.ts
@@ -0,0 +1,12 @@
+import { loadAsBlob } from "esm-resource";
+
+const loadRubberbandWasm = async () => {
+  const rubberbandBlob = await loadAsBlob({ importMeta: import.meta, filepath: "../node_modules/rubberband-wasm/dist/rubberband.wasm" });
+  const rubberbandWasmArrayBuffer = await rubberbandBlob.arrayBuffer();
+  const rubberbandWasm = new Uint8Array(rubberbandWasmArrayBuffer);
+  return rubberbandWasm;
+};
+
+export {
+  loadRubberbandWasm
+};
